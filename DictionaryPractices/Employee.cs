022dfedd79e98850d6f21a7e1facf838b7cdc15c
Josefin Lundquist <miss_lundquist@outlook.com>
2021-09-10 14:26:26 +0200

    prject with dictionary

---
+++ DictionaryPractices/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryPractices
+{
+    public class Employee
+    {
+
+        // enum to have constant values, that has to match
+        public enum Role
+        {
+            CEO,
+            CTO,
+            HRManager,
+            LeadTech,
+            JuniorTech
+        }
+
+        // Properties
+        public int EmpNumber { get;}
+        public string EmpName { get; set; }
+        public Role EmpRole { get; set; }
+        public int EmpYearsOfExperience { get; set; }
+        public const double EmpPay = 300000;
+
+        // Constructor
+        public Employee(int EmpNumber, string EmpName, Role EmpRole, int EmpYearsOfExperience)
+        {
+            this.EmpNumber = EmpNumber;
+            this.EmpName = EmpName;
+            this.EmpRole = EmpRole;
+            this.EmpYearsOfExperience = EmpYearsOfExperience;
+        }
+
+
+
+        // function of calculates the salery based by years of experience
+        public double saleryOfEmp(int EmpYearsOfExperience)
+        {
+            double convertedYearsOfExperience = EmpYearsOfExperience;
+            double EmpExpreienceCalculated = (convertedYearsOfExperience / 10) + 1.0;
+            Console.WriteLine(EmpExpreienceCalculated);
+            double calulatedSalaryByAge = EmpPay * EmpExpreienceCalculated;
+            return calulatedSalaryByAge;
+        }
+
+       
+        override public string ToString()
+        {
+            return EmpNumber + " " + EmpName + " " + EmpRole;
+        }
+    
+    }
+}
---
+++ DictionaryPractices/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List of employees
+            List<Employee> employeesList = new List<Employee>();
+            employeesList.Add(new Employee(1, "Josefin Lundquist", Employee.Role.CEO, 5));
+            employeesList.Add(new Employee(2, "Andres Canal", Employee.Role.CTO, 4));
+            employeesList.Add(new Employee(3, "Linus Lindh", Employee.Role.LeadTech, 3));
+
+            foreach (var employee in employeesList)
+            {
+                Console.WriteLine($"{employee.EmpRole} {employee.EmpName} {employee.saleryOfEmp(employee.EmpYearsOfExperience)}");
+            }
+
+
+            // Dictionary of the employees, Key is empNumber and value is the employee
+            IDictionary<int, Employee> empInfoDictonary = new Dictionary<int, Employee>();
+            foreach (var emp in employeesList)
+            {
+                empInfoDictonary.Add(emp.EmpNumber, emp);
+                //Console.WriteLine($"{emp.EmpName} {emp.EmpRole}");
+                Console.WriteLine(emp);
+            }
+
+            // to update a Dicotnary
+            int keyToUptdate = 3;
+            if(empInfoDictonary.ContainsKey(keyToUptdate))
+            {
+                Employee e = empInfoDictonary[keyToUptdate]; // storing the object of Linus in e 
+                e.EmpRole = Employee.Role.JuniorTech; //and updating it with a value
+
+                // this one creates a new object in the dic while in the list the old one is
+                // empInfoDictonary[keyToUptdate] = new Employee(3, "Linus Lindh", Employee.Role.JuniorTech, 1);
+            }
+
+            // here we see that Linus is now updated in both the list and dictionary
+            Console.WriteLine(employeesList[2]);
+            Console.WriteLine(empInfoDictonary[3]);
+
+        }
+    }
+}
